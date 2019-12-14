6228b0dceb927111c3995372e382e6752a3eeb8d
Mario Garrido <login@mariogarrido.pt>
2019-12-14 02:20:55 +0000

    first commit

---
+++ js/app.js
@@ -0,0 +1,49 @@
+(function() {
+    window[window.attachEvent ? "attachEvent" : "addEventListener"](window.attachEvent ? "onload" : "load", function () {
+        /* main calc functions */
+        var ratio = 16; // 1/16 ratio
+        var calculateCoffe = function(water) {
+            return Math.floor(water / ratio);
+        }
+
+        var calculateWater = function(coffee) {
+            return Math.floor(water * ratio);
+        }
+
+        var calculateWaterBloom = function(coffee) {
+            return Math.ceil(coffee * 2);
+        }
+
+        // update values on the page
+        var updateRender = function(water, coffee, bloom) {
+            console.log([water, coffee, bloom]);
+            document.getElementById('final-amount').value = water;
+            document.getElementById('coffee-amount').value = coffee;
+            document.getElementById('bloom-amount-info').innerHTML = bloom + "g";
+            document.getElementById('final-amount-info').innerHTML = water + "g";
+        }
+
+        // event change water value
+        document.getElementById('final-amount').addEventListener("keyup", function(){
+            var water = this.value
+            var coffee = calculateCoffe(water);
+            var bloom = calculateWaterBloom(coffee);
+
+            updateRender(water, coffee, bloom);
+        });
+
+        // event change coffe value
+        document.getElementById('coffee-amount').addEventListener("keyup", function(){
+            var coffee = this.value
+            var water = calculateWater(coffee);
+            var bloom = calculateWaterBloom(coffee);
+
+            updateRender(water, coffee, bloom);
+        });
+
+        // invert colors
+        document.getElementById('color-invert').addEventListener("click", function(){
+            document.getElementsByTagName('body')[0].classList.toggle('inverted-colors');
+        });
+    }, false);
+})();
