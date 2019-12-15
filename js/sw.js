d11ad4db11b5278fcc33ef727f460705e90366cc
Mario Garrido <mariogarrido@7graus.com>
2019-12-15 03:40:08 +0000

    WIP: set up a service worker for cache

---
+++ js/sw.js
@@ -0,0 +1,45 @@
+const staticCacheName = 'site-static-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/img/favicon.ico',
+  '/img/favicon.png',
+  '/img/icon-72x72.png',
+  '/img/icon-96x96.png',
+  '/img/icon-128x128.png',
+  '/img/icon-144x144.png',
+  '/img/icon-152x152.png',
+  '/img/icon-192x192.png',
+  '/img/icon-384x384.png',
+  '/img/icon-512x512.png',
+  'https://fonts.googleapis.com/css?family=Open+Sans:300,400,700&display=swap',
+];
+// install event
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log('caching shell assets');
+      cache.addAll(assets);
+    })
+  );
+});
+// activate event
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== staticCacheName)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+// fetch event
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request);
+    })
+  );
+});
